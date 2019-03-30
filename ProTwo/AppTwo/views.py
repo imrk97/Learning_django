5befabf1ee803bfb29b38877c82d21084c1ae539
Rohan Karmakar <rohankarmakar97@gmail.com>
2019-03-30 20:31:01 +0530

    challenge 2 complete

--- ProTwo/AppTwo/views.py
+++ ProTwo/AppTwo/views.py
@@ -2,4 +2,4 @@ from django.shortcuts import render
 from django.http import HttpResponse
 # Create your views here.
 def index(request):
-    return HttpResponse('<em>Hello World!</em>')
+    return HttpResponse('<em>My Second App</em>')
