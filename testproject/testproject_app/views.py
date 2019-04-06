6f19dc1c69846feda67cccc82269f0aa70f911bc
Rohan Karmakar <rohankarmakar97@gmail.com>
2019-04-06 22:25:05 +0530

    model view incomplte

--- testproject/testproject_app/views.py
+++ testproject/testproject_app/views.py
@@ -1,8 +1,11 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from testproject_app.models import Topic, Webpage, AccessRecord
 # Create your views here.
 
 
 def index(request):
-    my_dict = {'insert_me': "HELLO I AM FROM views.py !!"}
-    return render(request, 'testproject_app\index.html', context=my_dict)
+    webpages_list=Webpage.objects
+    date_dict={'access_record':webpages_list}
+    #my_dict = {'insert_me': "HELLO I AM FROM views.py !!"}
+    return render(request, 'testproject_app\index.html', context=date_dict)
